99aef7fbd9ce3eed0e24fcc5feb716650ca6b1a4
Erich Sickert <esickert@gmail.com>
2018-02-04 20:50:14 -0800

    plus

--- src/main/java/com/shipt/stuff/ShiptCodingTest.java
+++ src/main/java/com/shipt/stuff/ShiptCodingTest.java
@@ -54,6 +54,7 @@ public class ShiptCodingTest {
         WebElement coke = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]/div/div[1]/ion-item/div[1]/div[2]/button[2]"));
         String dietCoke = coke.getText();  //it's failing here!!!!!!!!!!!!!!
         System.out.println(dietCoke);
+        coke.click();
 
         WebElement name = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]/div/div[1]/ion-item/div[1]/p"));
         productName1 = name.getText();
