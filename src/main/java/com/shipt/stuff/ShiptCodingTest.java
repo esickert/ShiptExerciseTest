e9bbb20507e94b84c74dfaa65af1425bf02a217e
Erich Sickert <esickert@gmail.com>
2018-02-09 15:11:05 -0800

    plus

--- src/main/java/com/shipt/stuff/ShiptCodingTest.java
+++ src/main/java/com/shipt/stuff/ShiptCodingTest.java
@@ -1,16 +1,13 @@
 package com.shipt.stuff;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 
 import java.io.IOException;
-import java.util.List;
-import static java.awt.SystemColor.info;
 import static java.lang.Thread.sleep;
-import static javax.swing.text.html.CSS.getAttribute;
+import static org.hamcrest.core.Is.is;
 
 public class ShiptCodingTest {
 
@@ -20,58 +17,64 @@ public class ShiptCodingTest {
         String url = "http://www.shipt.com";
         String email = "qatest@shipt.com";
         String passwd = "Sh1pt123!";
-        String item = "coke zero";
+        String item = "milk";
         String productName1;
 
-        System.setProperty("webdriver.chrome.driver", "c://SeleniumDrivers//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to(url);
+        int count = 0;
+//        while (count <= 5) {
+            System.out.println(count);
+            System.setProperty("webdriver.chrome.driver", "c://SeleniumDrivers//chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.navigate().to(url);
 
 //        driver.manage().window().maximize();
-        driver.findElement(By.cssSelector(".button-secondary")).click();
+            driver.findElement(By.cssSelector(".button-secondary")).click();
 
-        WebElement userAddressBox = driver.findElement(By.cssSelector("input.ng-valid-email"));
-        userAddressBox.click();
-        userAddressBox.sendKeys(email);
+            WebElement userAddressBox = driver.findElement(By.cssSelector("input.ng-valid-email"));
+            userAddressBox.click();
+            userAddressBox.sendKeys(email);
 
-        WebElement passwdBox = driver.findElement(By.cssSelector("input.ng-invalid-required"));
-        passwdBox.click();
-        passwdBox.sendKeys(passwd);
+            WebElement passwdBox = driver.findElement(By.cssSelector("input.ng-invalid-required"));
+            passwdBox.click();
+            passwdBox.sendKeys(passwd);
 
-        driver.findElement(By.cssSelector("#start_shopping_login_button")).click();
-        sleep(5000);
-        WebElement search = driver.findElement(By.cssSelector("#search"));
-        search.click();
+            driver.findElement(By.cssSelector("#start_shopping_login_button")).click();
+            sleep(5000);
+            WebElement search = driver.findElement(By.cssSelector("#search"));
+            search.click();
 
 
-        search.sendKeys(item);
-        search.sendKeys(Keys.ENTER);
-        WebElement page = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]"));
+            search.sendKeys(item);
+            search.sendKeys(Keys.ENTER);
+            WebElement page = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]"));
 
-        sleep(3000);
-        page.click();
+            sleep(3000);
+            page.click();
 
-        WebElement coke = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]/div/div[1]/ion-item/div[1]/div[2]/button[2]"));
-        String dietCoke = coke.getText();  //it's failing here!!!!!!!!!!!!!!
-        System.out.println(dietCoke);
-        coke.click();
+            WebElement coke = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]/div/div[1]/ion-item/div[1]/div[2]/button[2]"));
+            String dietCoke = coke.getText();  //it's failing here!!!!!!!!!!!!!!
+            System.out.println(dietCoke);
+            coke.click();
 
-        WebElement name = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]/div/div[1]/ion-item/div[1]/p"));
-        productName1 = name.getText();
-        System.out.println(productName1);
-        coke.click();
-        sleep(5000);
+            WebElement name = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/div/div[1]/div[2]/div/div[1]/ion-item/div[1]/p"));
+            productName1 = name.getText();
+            System.out.println(productName1);
+            coke.click();
+            sleep(5000);
 
-//       WebElement cart = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/shipt-web-header/div/div/web-cart-button/button"));
-//       If (the fucking popup opens)
-//            kill it;
-//        else
-//            cart.click();
+            WebElement cart = driver.findElement(By.xpath("//*[@id=\"homeIonContent\"]/div/div/shipt-web-header/div/div/web-cart-button/button"));
+            System.out.println(cart.getText());
+            cart.click();
+            sleep(5000);
 
-       sleep(5000);
+            WebElement name2 = driver.findElement(By.cssSelector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-view > ion-view.web-shopping-cart.pane > ion-content > div > div > div.col.col-67 > div.cart-items-area > div > div.item.item-thumbnail-left.item-button-right.cart-list-item > div.row > div > p"));
+            String productName2 = name2.getText();
+            System.out.println(productName2);
 
-//        WebElement cartName = driver.findElement(By.xpath("/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-view/ion-view[2]/ion-content/div/div/div[1]/div[1]/div/div[2]/div[3]/div/p"));
-//        System.out.println(cartName.getText());
-
+            Assert.assertThat("confirm names are same", productName1, is(productName2));
+            sleep(5000);
+//            driver.quit();
+//            count++;
+//        }
     }
 }
